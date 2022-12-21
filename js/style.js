e84bf0e70ad25b647fedf2e6044aca14f05d9fff
Mancioh <Eliamancini23@gmail.com>
2022-12-21 15:56:20 +0100

    2nd

--- js/style.js
+++ js/style.js
@@ -4,3 +4,10 @@ console.log("JS OK");
 //  per i multipli di 3 stampi “Fizz” al posto del numero
 //  per i multipli di 5 stampi “Buzz” al posto del numero.
 //  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+// 1- prendo gli elementi dal dom
+// 2- creo un loop for da 1 a 100
+// 3- inserisco un if innestato per i multipli di 3
+// 4- inserisco un if innestato per i multipli di 5
+// 5- inserisco un if innestato per i multipli di 15
+// 6- stampo il risultato
